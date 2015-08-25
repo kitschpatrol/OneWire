be6e3080d277336fa2a51386bc939b2e02a75f77
Eric Mika <eric@ericmika.com>
2015-08-25 00:26:02 -0400

    Improve documentation to sequence detect subclass.

--- examples/DS28EA00_Sequence_Detect/DS28EA00_Sequence_Detect.ino
+++ examples/DS28EA00_Sequence_Detect/DS28EA00_Sequence_Detect.ino
@@ -3,16 +3,22 @@
 
 // OneWire DS28EA00 Sequence Detect Example
 //
-// The OneWire library's "search" method will return all devices on a bus, but not
-// necessaruly in the order they're actually attached. The DS28EA00 Theremometer implements 
+// The OneWire library's default "search" method will return all devices on a bus, but not
+// necessarily in the order they're actually attached. The DS28EA00 Thermometer implements 
 // a feature called Sequence Detect which allows OneWire devices to be enumerated in the actual
 // order they're physically attached.
 //
-//  The OneWireSequence subclass which encapsulates the sequence detection process. It imlements methods
-// with the same signatures and usage as the search function, but which return devices in order.
+// The OneWireSequence subclass which encapsulates the sequence detection process. It imlements methods
+// with the same signatures and usage as the search function, but which returns devices in their physically
+// attached order.
+// 
 // Read more about sequence detect here: http://www.maximintegrated.com/en/products/analog/sensors-and-sensor-interface/DS28EA00.html
 // Read more about the implementation on page 28 of datasheet: http://pdfserv.maximintegrated.com/en/ds/DS28EA00.pdf
 //
+// See the DS28EA00_Sequence_Detect.ino file for an example of this functionality in use:
+// https://github.com/kitschpatrol/OneWire/blob/master/examples/DS28EA00_Sequence_Detect/DS28EA00_Sequence_Detect.ino
+//
+// Documentation for the base library is abailable here:
 // http://www.pjrc.com/teensy/td_libs_OneWire.html
 
 OneWireSequence  ds(10);  // on pin 10 (a 4.7K resistor is necessary)
